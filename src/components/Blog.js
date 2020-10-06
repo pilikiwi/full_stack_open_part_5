105c29eb2c2f4abb6ae22dc4b61c2597d0ff3111
Hsu <ChihWeiHsu@dpss.lacounty.gov>
2020-10-06 13:59:56 -0700

    5.7* Blog list frontend, step7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,29 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} by {blog.author} With Likes: {blog.likes}
+import React, { useState } from 'react'
+
+const blogStyle ={
+  padding: 10,
+  border: 'dashed',
+  borderWidth: 1,
+  borderRadius: 25,
+  marginBottom: 5,
+}
+
+const Blog = ({ blog }) => {
+
+  const [visible, setVisible] = useState(false)
+
+  const showDetail = { display: visible ? 'none' : '' }
+  const hideDetail = { display: visible ? '' : 'none' }
+
+  return (
+  <div style={blogStyle}>
+    <h4>{blog.title} by {blog.author}</h4> 
+    <div style={showDetail}> <button onClick={() => setVisible(true)}>view</button> </div>
+    <div style={hideDetail}>
+    <p >With Likes: {blog.likes} <button>Like</button> <br/>@ {blog.url}</p>
+    <button onClick={() => setVisible(false)}>hide</button>
+    </div>
   </div>
-)
+)}
 
 export default Blog
